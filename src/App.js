721611db4d0584bb9786e4d656c29075d2e93a54
Jim Vickery <jimrv8@yahoo.com>
2018-08-05 18:56:36 -0500

    Render a js filed in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/projects';
+import Projects from './components/projects';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        My app
+       <Projects />
       </div>
     );
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class Projects extends Component {
   render() {
     return (
-      <div className="App">
-       My app
+      <div className="Projects">
+       My Projects
       </div>
     );
   }
 }
 
-export default App;
+export default Projects;
