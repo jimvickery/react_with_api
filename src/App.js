ba78051e7eed3013c760254312ab53ed5aaa34b5
Jim Vickery <jimrv8@yahoo.com>
2018-08-06 02:25:04 -0500

    added category dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import './App.css';
-import './components/projects';
-import Projects from './components/projects';
+import './components/Projects';
+import Projects from './components/Projects';
+import AddProject from './components/AddProject';
+import './App.css';  
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          title: 'Ecomerence Shopping Cart',
-          category: 'Web Development'
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [ 
+    {
+      title: 'Business Website',
+      category: 'Web Design'
+    },
+    {
+      title: 'Social App',
+      category: 'Mobile Development'
+    },
+    {
+      title: 'Ecomerence Shopping Cart',
+      category: 'Web Development'
+    }]})
   }
   render() {
     return (
       <div className="App">
-       My app
+       <AddProject />
        <Projects projects={this.state.projects} />
       </div>
     );
---
+++ src/components/AddProject.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  static defaultProps ={
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+
+  render() {
+    let cateoryOption = this.props.categories.map(category => {
+        return <option key={category} value="category">{category}</option>
+    });
+    return (
+      <div>
+        <h3>Add Project</h3>
+        <form>
+          <div> 
+            <label> Title </label> <br />
+            <input type="text" ref="title" />
+          </div>
+
+          <div> 
+            <label> Category </label> <br />
+            <select ref="category">
+              {cateoryOption}
+            </select>
+          </div>
+        </form>
+        <br />
+
+      </div>
+    );
+  }
+}
+
+export default AddProject;
