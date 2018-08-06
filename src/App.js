930569b1cb73ec96d7a89ba3cf213187230f69f3
Jim Vickery <jimrv8@yahoo.com>
2018-08-05 19:15:41 -0500

    added array object

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import './components/projects';
 import Projects from './components/projects';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecomerence Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
        My app
-       <Projects />
+       <Projects projects={this.state.projects} />
       </div>
     );
   }
